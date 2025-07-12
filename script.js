9d1849fa6731175ed1b32d9bd8139eea958a86ba
Vincent <Vincentliu8823@gmail.com>
2025-07-12 11:27:52 -0400

    made buttons clickable without making container clickable

--- script.js
+++ script.js
@@ -43,36 +43,6 @@ function operate(operator, a, b)
 };
 
 
-let a = 6;
-let b = 3;
-
-// console.log(add(a, b)); // returns 9
-// console.log(add(-a, b)); // returns -3
-console.log(operate('+', a, b)); // returns 9
-console.log(operate('+', -a, b)); // returns -3
-
-
-// console.log('\n' + subtract(a, b)); // 3
-// console.log(subtract(-a, b)); // -9
-console.log('\n' + operate('-', a, b));
-console.log(operate('-', -a, b));
-
-
-// console.log('\n' + multiply(a, b)); // 18
-// console.log(multiply(-a, b)); // -18
-console.log('\n' + operate('*', a, b)); 
-console.log(operate('*', -a, b));
-
-
-// console.log('\n' + divide(a, b)); // 2
-// console.log(divide(-a, b)); // -2
-console.log('\n' + operate('/', a, b));
-console.log(operate('/', -a, b));
-
-
-
-// trying to make buttons onclick
-let calc = document.querySelector('.calculator');
 // making the output screen interactable
 let output = document.querySelector('.output');
 
@@ -86,69 +56,44 @@ let num1 = 0;
 let num2 = 0;
 let operater;
 
-// using the bubble feature so I don't need to make a onClick on all the buttons
-calc.addEventListener('click', (e) => 
+
+// trying to make buttons onClick
+let numBtn = document.querySelectorAll('.num');
+numBtn.forEach((button) => 
 {
-    let str = e.target.innerText; // gets the text of which button that was clicked
-
-    if (numbers.includes(str)) // checks if the button clicked is a number or decimal
+    button.addEventListener('click', () => 
     {
-        console.log('this is a number');
-
-        let newStr; // used to store the new string that includes the new number at the end
-
-        // 0 is defult value for output screen, so if only 0, don't include it
-        if (output.textContent === '0') 
-        {
-            if (str === '.') // keep 0 if first input is a decimal
-               newStr = output.textContent + str; 
-            else
-                newStr = str; // replace 0 with number
-        }
-        else
-        {
-        
-            console.log(str + " a number");
-            
-            newStr = output.textContent + str; // adds the number to the end
-        }
-        
-        console.log(newStr);
-
-        output.textContent = newStr;
-    }
-    else // if not a number or decimal
-    {
-        console.log(str);
-
-        if (str === 'Clear') // maybe put clear, equal, & pos/neg into a switch statement
-            output.textContent = '0';
-        else
-        {
-            if (operaters.includes(str)) // if input was an operator
-            {
-                console.log('this is an operator');
-
-                num1 = Number(output.textContent);
-                operater = str;
-                //console.log('prev num: ' + num1);
-                output.textContent = '';
-            }
-            else if (str === '=')
-            {
-                num2 = Number(output.textContent);
-                let result = operate(operater, num1 , num2);
-                console.log(`num1: ${num1}, num2: ${num2}, operator: ${operater}, = ` + result);
-                output.textContent = result;
-            }
-            else
-                output.textContent = str;
-        }
-            
-    }
+        output.textContent = button.textContent;
+        console.log(button.textContent + ' is a number');
+    });
+    
 });
 
-console.log("poo " + operate('+', 1, 2));
+
+// trying to make operators onClick
+let operaterBtn = document.querySelectorAll('.operator');
+operaterBtn.forEach((button) => 
+{
+    button.addEventListener('click', () => 
+    {
+        output.textContent = button.textContent;
+        console.log(button.textContent + ' is an operator')
+    });
+});
+
+
+// trying to make other btns onClick
+let otherBtn = document.querySelectorAll('.otherBtn');
+otherBtn.forEach((button) => 
+{
+    button.addEventListener('click', () => 
+    {
+        output.textContent = button.textContent;
+        console.log(button.textContent + ' is an misc')
+    });
+});
+
+
 
 
 
