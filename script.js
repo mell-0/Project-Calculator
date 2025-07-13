6d5b1b41fb13427032e9e2209af48ec976171fef
Vincent <Vincentliu8823@gmail.com>
2025-07-12 22:30:58 -0400

    decimal output now works

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function divide(a, b)
 
 function operate(operator, a, b)
 {
-    console.log(`hi from operator ${operator} ${a} ${b}`);
+    //console.log(`hi from operator ${operator} ${a} ${b}`);
     switch (operator)
     {
         case '+':
@@ -50,7 +50,8 @@ let output = document.querySelector('.output');
 // used to store the values for the operator function
 let num1 = 0;
 let num2 = 0;
-let operater;
+let operater = undefined;
+let answer = undefined;
 
 let digits = 0; // used to count # of digits in a num, only allow 14 digits
 
@@ -75,21 +76,72 @@ numBtn.forEach((button) =>
             }
         }
         console.log(button.textContent + ' is a number');
+
+        // storing numbers if an operator was clicked
+        if (operater === undefined)
+            num1 = output.textContent;
+        else
+            num2 = output.textContent;
     });
+});
+
+
+// getting decimal button
+let decimalBtn = document.querySelector('.decimal');
+
+decimalBtn.addEventListener('click', () =>
+{
+    console.log("this is a deciaml");
     
+    if (!output.textContent.includes(".")) // only adds decimal when it doesn't have one
+    {
+        if (output.textContent === '')
+            output.textContent = '0.';
+        else
+            output.textContent += '.';
+
+        digits++;
+    }
 });
 
 
 // trying to make operators onClick
+// first try to make it work on the console
 let operaterBtn = document.querySelectorAll('.operator');
 operaterBtn.forEach((button) => 
 {
     button.addEventListener('click', () => 
     {
-        output.textContent = button.textContent;
+        operater = button.textContent; // store the operator
+        output.textContent = ''; // clear output when an operator is clicked on
+        digits = 0;
+
         console.log(button.textContent + ' is an operator');
     });
 });
+
+
+// getting equal button to work
+let equalBtn = document.querySelector('.equal');
+equalBtn.addEventListener('click', (e) => 
+{
+    console.log(e.target.textContent + " was clicked");
+    console.log(`${num1} ${operater} ${num2} = ${operate(operater, Number(num1), Number(num2))}`);
+    
+    answer = parseFloat(operate(operater, Number(num1), Number(num2)).toFixed(3));
+
+    // round to 3rd place if answer len is more than 13
+    if (String(answer).length > 13)
+    {
+        output.textContent = Number.parseFloat(answer).toExponential(3); // rounds to the 3rd place
+    }
+    else
+    {   // if answer length is less than or equal to 13
+        output.textContent = answer; 
+    }
+    
+});
+
 
 
 // trying to make other btns onClick
@@ -104,19 +156,6 @@ otherBtn.forEach((button) =>
 });
 
 
-// getting decimal button
-let decimalBtn = document.querySelector('.decimal');
-
-decimalBtn.addEventListener('click', () =>
-{
-    console.log("this is a deciaml");
-    
-    if (!output.textContent.includes(".")) // only adds decimal when it doesn't have one
-    {
-        output.textContent += '.';
-        digits++;
-    }
-});
 
 
 
